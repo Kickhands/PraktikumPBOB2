0755f990a8c0231798cd5975ba2df2867852008b
CallMeHaidar <117249249+Kickhands@users.noreply.github.com>
2024-02-20 14:34:15 +0700

    Update Mtitik.java

--- PraktikumSatu/Mtitik.java
+++ PraktikumSatu/Mtitik.java
@@ -1,15 +1,19 @@
 public class Mtitik {
-    static double counter;
     public static void main(String[] args) {
-        Titik t1 = new Titik(1, 2);
-        Titik t2 = new Titik(3, 4);
-        Titik t3 = new Titik();
+        Titik t1 = new Titik();
+        t1.setAbsis(1);
+        t1.setOrdinat(2);
 
-        t3.setAbsis(5);
-        t3.setOrdinat(6);
-        System.out.println("t1: " + t1.getAbsis()+ ", " + t1.getOrdinat());
-        System.out.println("t2: " + t2.getAbsis()+ ", " + t2.getOrdinat());
-        System.out.println("t3: " + t3.getAbsis()+ ", " + t3.getOrdinat());
-        System.out.println("Jumlah objek titik yang telah dibuat: " + Titik.getCounter());
+        Titik t2 = new Titik();
+        t2.setAbsis(3);
+        t2.setOrdinat(4);
+
+        Titik t3 = new Titik(2,3);
+
+        System.out.println("jumlah objek titik : " + Titik.getCounterTitik());
+
+        System.out.println("t1(" + t1.getAbsis() + "," + t1.getOrdinat() + ")");
+        System.out.println("t2(" + t2.getAbsis() + "," + t2.getOrdinat() + ")");
+        System.out.println("t3(" + t3.getAbsis() + "," + t3.getOrdinat() + ")");
     }
 }
