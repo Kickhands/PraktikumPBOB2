b418a5a8f5c25ca70cf0eab90d639c3e1c24b921
CallMeHaidar <117249249+Kickhands@users.noreply.github.com>
2024-03-06 22:41:55 +0700

    Update ExceptionOnArray.java

--- Praktikum3/ExceptionOnArray.java
+++ Praktikum3/ExceptionOnArray.java
@@ -1,3 +1,8 @@
+//File      : ExceptionOnArray.java
+//Nama      : GIGIH HAIDAR FALAH
+//NIM       : 24060122140150
+//Tanggal   : 05/03/2024
+
 public class ExceptionOnArray {
     public static void main(String[] args) {
         integer[] arrayInteger = new Integer[4] ;
