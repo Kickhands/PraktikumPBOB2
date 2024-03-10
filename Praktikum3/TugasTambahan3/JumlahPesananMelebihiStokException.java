5d26046c4675c86ffa64d135e0a6e31015508d8d
CallMeHaidar <117249249+Kickhands@users.noreply.github.com>
2024-03-10 22:29:47 +0700

    Delete Praktikum3/TugasTambahan3 directory

--- Praktikum3/TugasTambahan3/JumlahPesananMelebihiStokException.java
+++
@@ -1,9 +0,0 @@
-//NAMA      : GIGIH HAIDAR FALAH
-//NIM       : 24060122140150
-//LAB       : B2
-//PRAKTIKUM3
-public class JumlahPesananMelebihiStokException extends Exception {
-    public JumlahPesananMelebihiStokException(String message) {
-        super(message); // Panggil konstruktor parent dengan keyword super
-    }
-}
