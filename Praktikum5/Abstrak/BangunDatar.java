6685c659a11c8df720bf4fd72c266ddc219b6885
CallMeHaidar <117249249+Kickhands@users.noreply.github.com>
2024-03-20 21:27:59 +0700

    Create BangunDatar.java

---
+++ Praktikum5/Abstrak/BangunDatar.java
@@ -0,0 +1,20 @@
+/*
+ * Penulis : Gigih Haidar Falah
+ * Deskripsi : kelas abstrak, berisi abstraksi bangun datar
+ * Nama File : BangunDatar.java
+ */
+
+ 
+ public abstract class BangunDatar{
+    protected double luas;
+
+    public abstract double hitungLuas (double sisi);
+
+    public void setLuas(double l){
+        luas = 1;
+    }
+
+    public double getLuas(){
+        return luas;
+    }
+}
