226b8a84718e5cce49363ecd84f5303e8de2214b
CallMeHaidar <117249249+Kickhands@users.noreply.github.com>
2024-03-20 21:30:35 +0700

    Add files via upload

---
+++ Praktikum5/Interface/IArea.java
@@ -0,0 +1,9 @@
+/*
+ * Penulis : Gigih Haidar Falah
+ * Deskripsi : interface yang mendefinisikan kontrak bahwa setiap kelas yang membuat implementasi dar interface ini harus memiliki cara untuk menghitung luas
+ * Nama File : IArea.java
+ */
+
+ public interface IArea {
+    public double hitungLuas();
+}
